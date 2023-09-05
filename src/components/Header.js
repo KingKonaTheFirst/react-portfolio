89e8e7205df96d4d5f7491215057e48739d486be
Kona <landon.deroin@gmail.com>
2023-09-05 19:27:39 -0400

    fixed imports from header to nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,8 @@
 import React from 'react';
+// import nav icons
+import { BiHomeAlt, BiUser } from 'react-icons/bi';
+import { BsClipboardData, BiBriefcase, BsChatSquare } from 'react-icons/bs';
+import { Link } from 'react-scroll';
 
 const Header = () => {
   return <div>Header</div>;
